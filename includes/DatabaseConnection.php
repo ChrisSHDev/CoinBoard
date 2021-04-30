73715ade61ff2de56dfb4633b99f774cddde6dcd
ChrisSHDev <kakeochee@naver.com>
2021-04-30 14:12:59 -0700

    connected database

--- includes/DatabaseConnection.php
+++ includes/DatabaseConnection.php
@@ -1,6 +1,9 @@
-<?php 
+<?php
 
-$pdo = new PDO( 'mysql:host=localhost;dbname=coindb;charset=utf8',
-'coindbuser', 'ilovecanada');
+$pdo = new PDO(
+    'mysql:host=us-cdbr-east-03.cleardb.com;dbname=heroku_85647ba5e83802f;charset=utf8',
+    'b362fedcb8db41',
+    '9edd129b'
+);
 
 $pdo -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
