28f544521476e96f2625079ff80fd1055399969a
ChrisSHDev <kakeochee@naver.com>
2021-05-02 14:14:38 -0700

    added bootstrap class to style pags

--- templates/login.html.php
+++ templates/login.html.php
@@ -4,14 +4,18 @@ if (isset($error)):
 endif;
 ?>
 
-<form action="" method="post">
-  <label for="email">Email: </label>
-  <input type="text" name="email" id="email">
+<div class="form-box contain d-flex justify-content-center mt-5 mb-5">
+  <div class="row col-lg-6 rounded border border-2 p-3">
+    <form action="" method="post">
+      <label for="email" class="form-label">Email: </label>
+      <input type="text" name="email" id="email" class="form-control mt-2 mb-2">
 
-  <label for="password">Password: </label>
-  <input type="password" name="password" id="password">
+      <label for="password" class="form-label">Password: </label>
+      <input type="password" name="password" id="password" class="form-control mt-2 mb-2">
 
-  <input type="submit" name="submit" value="Log In">
-</form>
+      <input type="submit" name="submit" value="Log In" class="btn btn-sm bg-darker  mt-2 mb-2">
+    </form>
 
-<p>No Account? <a href="/user/register">Sing-in</a> now!</p>
+    <p>No Account? <a href="/user/register">Sing-in</a> now!</p>
+  </div>
+</div>
--- templates/single-article.html.php
+++ templates/single-article.html.php
@@ -1,38 +1,40 @@
-<div class="article-box">
-  <div class="article-subject">
-    <h1>
-      <?php echo htmlspecialchars($article['articlesubject'], ENT_QUOTES, 'UTF-8')  ?>
-    </h1>
-  </div>
-  <div class="article-list-box d-flex justify-content-between">
-    <ul class="list-group list-group-horizontal">
-      <li class="list-group-item d-flex justify-content-between align-items-center">
-        <?php echo htmlspecialchars($user, ENT_QUOTES, 'UTF-8')  ?>
-      </li>
-      <li class="list-group-item d-flex justify-content-between align-items-center">
-        <div class="table-date-title"> Date </div>
-        <div><span class="badge bg-primary rounded-pill">
-            <?php echo htmlspecialchars($article['articledate'], ENT_QUOTES, 'UTF-8')  ?></span>
-        </div>
-      </li>
-    </ul>
-    <ul class="list-group list-group-horizontal">
-      <li class="list-group-item d-flex justify-content-between align-items-center">
-        Like
-        <div><span class="badge bg-primary rounded-pill">1</span></div>
-      </li>
-      <li class="list-group-item d-flex justify-content-between align-items-center">
-        Dislike
-        <div><span class="badge bg-primary rounded-pill">1</span></div>
-      </li>
-      <li class="list-group-item d-flex justify-content-between align-items-center">
-        Comment
-        <div><span class="badge bg-primary rounded-pill">1</span></div>
-      </li>
-    </ul>
+<div class="row col-lg-8 justify-content-center m-auto pt-5 pb-5">
+  <div class="article-box">
+    <div class="article-subject">
+      <h1>
+        <?php echo htmlspecialchars($article['articlesubject'], ENT_QUOTES, 'UTF-8')  ?>
+      </h1>
+    </div>
+    <div class="article-list-box d-flex justify-content-between">
+      <ul class="list-group list-group-horizontal">
+        <li class="list-group-item d-flex justify-content-between align-items-center">
+          <?php echo htmlspecialchars($user, ENT_QUOTES, 'UTF-8')  ?>
+        </li>
+        <li class="list-group-item d-flex justify-content-between align-items-center">
+          <div class="table-date-title"> Date </div>
+          <div><span class="badge bg-primary rounded-pill">
+              <?php echo htmlspecialchars($article['articledate'], ENT_QUOTES, 'UTF-8')  ?></span>
+          </div>
+        </li>
+      </ul>
+      <ul class="list-group list-group-horizontal">
+        <li class="list-group-item d-flex justify-content-between align-items-center">
+          Like
+          <div><span class="badge bg-primary rounded-pill">1</span></div>
+        </li>
+        <li class="list-group-item d-flex justify-content-between align-items-center">
+          Dislike
+          <div><span class="badge bg-primary rounded-pill">1</span></div>
+        </li>
+        <li class="list-group-item d-flex justify-content-between align-items-center">
+          Comment
+          <div><span class="badge bg-primary rounded-pill">1</span></div>
+        </li>
+      </ul>
 
-  </div>
-  <div class="article-body">
-    <?php echo htmlspecialchars($article['articlecontents'], ENT_QUOTES, 'UTF-8')  ?>
+    </div>
+    <div class="article-body">
+      <?php echo htmlspecialchars($article['articlecontents'], ENT_QUOTES, 'UTF-8')  ?>
+    </div>
   </div>
 </div>
